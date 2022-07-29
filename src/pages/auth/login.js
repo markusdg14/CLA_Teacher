ad057d2c015ef9152b851517b034e9aa9c2d8ff8
markusdg14 <markusdg14@gmail.com>
2022-07-29 12:46:21 +0700

    auth url

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -53,9 +53,6 @@ export default function AuthLogin(){
     }
 
     async function signInBtn(){
-        localStorage.setItem('token', 'Bearer 888|uJMgoMHG8lDq1HK1KQJwcXTbzf4PvKkQR3DNSNRB')
-        window.location.href = '/'
-
         var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
         var flag = 1
         var data_login = {email : '', password : '', token : firebaseToken}
